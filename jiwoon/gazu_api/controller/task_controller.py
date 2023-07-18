a4cd65bcc809e3019956b64022f651055554d158
jiwothur <jiwothur@naver.com>
2023-07-18 15:25:59 +0900

    main_ui 추가

--- jiwoon/gazu_api/controller/task_controller.py
+++ jiwoon/gazu_api/controller/task_controller.py
@@ -12,11 +12,11 @@ class Controller:
         self.view = view
         # self.shot_service = ShotService(model, view)
         self.task_service = TaskService(model, view)
-        self.view.listView.setModel(model)
+        # self.view.listView.setModel(model)
         self.view.show()
 
         # events & slots
-        self.view.loadButton.clicked.connect(self.task_service.load_shot)
+        # self.view.loadButton.clicked.connect(self.task_service.load_shot)
         # self.view.addButton.clicked.connect(self.service.create_asset)
         # self.view.modifyButton.clicked.connect(self.service.update_asset)
         # self.view.deleteButton.clicked.connect(self.service.remove_asset)
--- jiwoon/gazu_api/view/task_view.py
+++ jiwoon/gazu_api/view/task_view.py
@@ -3,10 +3,10 @@ import os
 from PySide2 import QtCore, QtUiTools
 from PySide2.QtWidgets import QMainWindow
 
-from jiwoon.gazu_api.view.UI.asset_ui_2 import Ui_MainWindow
+from jiwoon.gazu_api.view.UI.nuki_main_widget import Ui_Nuki
 
 
-class MainUI(QMainWindow, Ui_MainWindow):
+class MainUI(QMainWindow, Ui_Nuki):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
