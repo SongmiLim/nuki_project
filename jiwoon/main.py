6848db7f78484bae72481b10431ef6bb508edb8b
songmilim <lsmi121@naver.com>
2023-07-18 15:49:07 +0900

    shot, task 기능 분리 기존 task_service에 있던 shot 관련 기능들을 shot_service.py 파일로 기능 분리

---
+++ jiwoon/main.py
@@ -0,0 +1,16 @@
+import sys
+from PySide2 import QtWidgets
+
+from jiwoon.gazu_api.controller.task_controller import Controller
+from jiwoon.gazu_api.model.task_model import Model
+from jiwoon.gazu_api.view.login import Login
+from jiwoon.gazu_api.view.task_view import MainUI
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+# add nuki Class later(for auth)
+UI = MainUI()
+# UI = Login()
+
+gz = Controller(Model(), UI)
+sys.exit(app.exec_())
