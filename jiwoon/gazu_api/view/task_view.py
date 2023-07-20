205afe1574d8a51a173fdb8e0bc645680c041bdf
songmilim <lsmi121@naver.com>
2023-07-20 17:28:00 +0900

    shot_list 정렬 변경

--- jiwoon/gazu_api/view/task_view.py
+++ jiwoon/gazu_api/view/task_view.py
@@ -1,7 +1,7 @@
 import os
 
 from PySide2 import QtCore, QtUiTools
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QListView
 
 from jiwoon.gazu_api.view.UI.nuki_main_widget import Ui_Nuki
 
@@ -13,3 +13,8 @@ class MainUI(QMainWindow, Ui_Nuki):
 
         # UI
         self.shot_list.setStyleSheet('background-color: transparent')
+        self.shot_list.setViewMode(QListView.IconMode)
+        self.shot_list.setIconSize(QtCore.QSize(400, 200))
+        # self.shot_list.setUniformItemSizes(True)
+        self.shot_list.setGridSize(QtCore.QSize(180, 140))
+        # self.shot_list.setMovement(QListView.Static)
