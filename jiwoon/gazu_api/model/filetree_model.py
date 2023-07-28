751923a2f779dc20bea195bdc18b2bf18f3768ff
dokyung <tumemanque00@gmail.com>
2023-07-28 13:23:15 +0900

    add compositing task만 보여주기, tooltip

--- jiwoon/gazu_api/model/filetree_model.py
+++ jiwoon/gazu_api/model/filetree_model.py
@@ -15,40 +15,52 @@ class TreeModel(QStandardItemModel):
     def __init__(self):
         super().__init__()
 
+
     def production_tree(self):
         self.clear()
         tasks = gazu.user.all_tasks_to_do()
         project_list = []
         for i in tasks:
-            self.project = None
-            self.projectItem = QStandardItem(i['project_name'])
-            if i['project_name'] not in project_list:
-                project_list.append(i['project_name'])
-                self.project = i['project_name']
-                self.appendRow(self.projectItem)
-                self.sequence_tree()
+            comp_task = i['task_type_name']
+            if comp_task == 'Compositing':  # Compositing task 만 view 에 보여주기
+                self.project = None
+                self.projectItem = QStandardItem(i['project_name'])
+                if i['project_name'] not in project_list:
+                    project_list.append(i['project_name'])
+                    self.project = i['project_name']
+                    self.appendRow(self.projectItem)
+                    self.sequence_tree()
+                if len(i['project_name']) > 18: # project name 글자가 길 경우 라벨로 풀 네임 보여주기
+                    self.projectItem.setToolTip(i['project_name'])
+
 
     def sequence_tree(self):
         tasks = gazu.user.all_tasks_to_do()
         self.seq_list = []
         for i in tasks:
-            self.sequence_list = []
-            self.seqItem = QStandardItem(i['sequence_name'])
-            if i['project_name'] == self.project and i['sequence_name'] not in self.seq_list:
-                self.seq_list.append(i['sequence_name'])
-                self.sequence_list.append(i['sequence_name'])
-                self.projectItem.appendRow(self.seqItem)
-                self.shot_tree()
+            comp_task = i['task_type_name']
+            if comp_task == 'Compositing':  # Compositing task 만 view 에 보여주기
+                self.sequence_list = []
+                self.seqItem = QStandardItem(i['sequence_name'])
+                if i['project_name'] == self.project and i['sequence_name'] not in self.seq_list:
+                    self.seq_list.append(i['sequence_name'])
+                    self.sequence_list.append(i['sequence_name'])
+                    self.projectItem.appendRow(self.seqItem)
+                    self.shot_tree()
+
 
     def shot_tree(self):
         tasks = gazu.user.all_tasks_to_do()
         for i in tasks:
-            self.shot_list = []
-            self.shotItem = QStandardItem(i['entity_name'])
-            if i['project_name'] == self.project and i['sequence_name'] in self.sequence_list and i[
-                'entity_name'] not in self.shot_list:
-                self.shot_list.append(i['entity_name'])
-                self.seqItem.appendRow(self.shotItem)
+            comp_task = i['task_type_name']
+            if comp_task == 'Compositing': # Compositing task 만 view 에 보여주기
+                self.shot_list = []
+                self.shotItem = QStandardItem(i['entity_name'])
+                if i['project_name'] == self.project and i['sequence_name'] in self.sequence_list and i[
+                    'entity_name'] not in self.shot_list:
+                    self.shot_list.append(i['entity_name'])
+                    self.seqItem.appendRow(self.shotItem)
+
 
     def headerData(self, section, orientation, role):
         if (orientation == Qt.Horizontal and role == Qt.DisplayRole):
@@ -60,7 +72,7 @@ class TreeModel(QStandardItemModel):
                     return user_id[0]
 
 
-    def select_filetree(self, index):
+    def select_filetree(self, index): # 로컬 파일에 연결 함수
         self.item = self.itemFromIndex(index)
         path_list = []
         shot_name = self.item.text()  # 샷 단계
