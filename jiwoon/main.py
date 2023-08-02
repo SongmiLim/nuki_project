66c3b50995a397faf0bb01accd634aee8849d77e
jiwothur <jiwothur@naver.com>
2023-08-02 10:00:50 +0900

    fix main

--- jiwoon/main.py
+++ jiwoon/main.py
@@ -2,18 +2,18 @@ import sys
 from PySide2 import QtWidgets, QtCore
 from jiwoon.gazu_api.view.UI.nuki import Nuki
 from PySide2.QtWidgets import QApplication, QMainWindow, QLineEdit, QWidget, QPushButton, QVBoxLayout
-class foo(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__()
-        self.Init()
-    def Init(self):
-        nuki_app = Nuki()
+# class foo(QMainWindow):
+#     def __init__(self, parent=None):
+#         super().__init__()
+#         self.Init()
+#     def Init(self):
+#         nuki_app = Nuki()
 
 
 
-# if __name__ == "__main__":
-#     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-#     app = QtWidgets.QApplication(sys.argv)
-#     apps = Nuki()
-#     sys.exit(app.exec_())
+if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    app = QtWidgets.QApplication(sys.argv)
+    apps = Nuki()
+    sys.exit(app.exec_())
 
