70784e4e94257bd8f525f4df8d5c0fd8ab21c9b1
SongmiLim <lsmi121@naver.com>
2023-08-21 11:38:47 +0900

    main 수정

--- nukitsu/main.py
+++ nukitsu/main.py
@@ -1,8 +1,19 @@
-from nukitsu.gazu_api.view.UI.nuki import Nuki
+import sys
+from PySide2 import QtWidgets, QtCore
 from PySide2.QtWidgets import QMainWindow
-
+from nukitsu.gazu_api.service.nuki import Nuki
 
 class NukiApp(QMainWindow):
     def __init__(self):
         super().__init__()
         Nuki()
+
+
+if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    if not QtWidgets.QApplication.instance():
+        app = QtWidgets.QApplication(sys.argv)
+    else:
+        app = QtWidgets.QApplication.instance()
+    apps = NukiApp()
+    sys.exit(app.exec_())
